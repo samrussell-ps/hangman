e70697aaf000918749b22c3d1d29ecb2414c000c
Sam Russell <sam.russell@powershop.co.nz>
2015-09-22 17:57:16 +1200

    tests pass, refactor time
    - nest tests that reuse Hangman
    - use let instead of before
    - create a random word generator class (collaborator)
    - create a game manager?

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,28 +1,56 @@
 require 'set'
 
 class Hangman
-  def initialize(initial_word = '')
-    @word_status = []
+  WORDLIST_FILENAME = './data/wordsEn.txt'
+  INITIAL_NUMBER_OF_LIVES = 9
+  def initialize(initial_word)
+    # TODO: doesn't check for non-alpha characters
+    # TODO: doesn't check for non-string or 0-length string
+    # TODO: doesn't enforce uppercase
+    @word = initial_word
+    @masked_letters = (1..@word.size).map{nil}
+    @guessed_letters = Set.new
   end
 
-  attr_reader :word_status
+  attr_reader :masked_letters
+
+  def get_number_of_words
+    File.readlines('./data/wordsEn.txt').size
+  end
+
+  def get_random_word
+    File.readlines('./data/wordsEn.txt')[rand(get_number_of_words)]
+  end
 
   def lives_left
+    return INITIAL_NUMBER_OF_LIVES - @guessed_letters.size + (@masked_letters.uniq - [nil]).size
   end
 
-  def has_letter_been_guessed
+  def has_letter_been_guessed?
   end
 
   def guess_letter(letter)
+    @guessed_letters.add(letter)
+    if @word.include?(letter)
+      @guessed_letters.add(letter)
+      @word.split('').each_with_index do |val, index|
+        if val == letter
+          @masked_letters[index] = letter
+        end
+      end
+    end
   end
 
-  def game_won
+  def game_won?
+    !@masked_letters.include?(nil)
   end
 
-  def game_lost
+  def game_lost?
+    lives_left == 0
   end
 
-  def game_over
+  def game_over?
+    game_won? || game_lost?
   end
 end
 
--- spec/hangman_spec.rb
+++ spec/hangman_spec.rb
@@ -2,19 +2,6 @@ require "hangman"
 
 RSpec.describe Hangman do
 
-  shared_examples 'tautologies' do |hangman|
-    describe '#game_lost? && #game_won?' do
-      it 'is false' do
-        expect(hangman.game_lost? && hangman.game_won?).to eq(false)
-      end
-    end
-    describe '#game_lost? || #game_won? == #game_over?' do
-      it 'is false' do
-        expect(hangman.game_lost? || hangman.game_won?).to eq(hangman.game_over?)
-      end
-    end
-  end
-
   context 'new Hangman object' do
     before do
       @hangman = Hangman.new('TELEPHONE')
@@ -24,9 +11,9 @@ RSpec.describe Hangman do
         expect(@hangman.lives_left).to eq(9)
       end
     end
-    describe '#word_status' do
+    describe '#masked_letters' do
       it 'is an array of nil' do
-        expect(@hangman.word_status).to match_array([nil])
+        expect(@hangman.masked_letters.uniq).to match_array([nil])
       end
     end
     describe '#game_won?' do
@@ -43,9 +30,6 @@ RSpec.describe Hangman do
       it 'is false' do
         expect(@hangman.game_over?).to eq(false)
       end
-    end
-    describe 'tautologies' do
-      include_examples 'tautologies', @hangman
     end
   end
 
@@ -60,9 +44,9 @@ RSpec.describe Hangman do
         expect(@hangman.lives_left).to eq(9)
       end
     end
-    describe '#word_status' do
+    describe '#masked_letters' do
       it 'is an array of a mix of nil and the letter guessed' do
-        expect(@hangman.word_status).to match_array([nil] + @letters_guessed)
+        expect(@hangman.masked_letters.uniq).to match_array([nil] + @letters_guessed)
       end
     end
     describe '#game_won?' do
@@ -79,9 +63,6 @@ RSpec.describe Hangman do
       it 'is false' do
         expect(@hangman.game_over?).to eq(false)
       end
-    end
-    describe 'tautologies' do
-      include_examples 'tautologies', @hangman
     end
   end
 
@@ -96,9 +77,9 @@ RSpec.describe Hangman do
         expect(@hangman.lives_left).to eq(8)
       end
     end
-    describe '#word_status' do
+    describe '#masked_letters' do
       it 'is an array of nil' do
-        expect(@hangman.word_status).to match_array([nil])
+        expect(@hangman.masked_letters.uniq).to match_array([nil])
       end
     end
     describe '#game_won?' do
@@ -116,9 +97,6 @@ RSpec.describe Hangman do
         expect(@hangman.game_over?).to eq(false)
       end
     end
-    describe 'tautologies' do
-      include_examples 'tautologies', @hangman
-    end
   end
 
   context 'Hangman object with 9 incorrect guesses' do
@@ -132,9 +110,9 @@ RSpec.describe Hangman do
         expect(@hangman.lives_left).to eq(0)
       end
     end
-    describe '#word_status' do
+    describe '#masked_letters' do
       it 'is an array of nil' do
-        expect(@hangman.word_status).to match_array([nil])
+        expect(@hangman.masked_letters.uniq).to match_array([nil])
       end
     end
     describe '#game_won?' do
@@ -152,12 +130,9 @@ RSpec.describe Hangman do
         expect(@hangman.game_over?).to eq(true)
       end
     end
-    describe 'tautologies' do
-      include_examples 'tautologies', @hangman
-    end
   end
 
-  shared_context 'Hangman object with 7 correct guesses, word complete' do
+  context 'Hangman object with 7 correct guesses, word complete' do
     before do
       @hangman = Hangman.new('TELEPHONE')
       @letters_guessed = ['T', 'E', 'L', 'P', 'H', 'O', 'N']
@@ -168,9 +143,9 @@ RSpec.describe Hangman do
         expect(@hangman.lives_left).to eq(9)
       end
     end
-    describe '#word_status' do
+    describe '#masked_letters' do
       it 'is an array of the letters guessed' do
-        expect(@hangman.word_status).to match_array(@letters_guessed)
+        expect(@hangman.masked_letters.uniq).to match_array(@letters_guessed)
       end
     end
     describe '#game_won?' do
@@ -187,9 +162,6 @@ RSpec.describe Hangman do
       it 'is true' do
         expect(@hangman.game_over?).to eq(true)
       end
-    end
-    describe 'tautologies' do
-      include_examples 'tautologies', @hangman
     end
   end
 
