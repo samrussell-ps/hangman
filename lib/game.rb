d5d06b7f1569c397254d96d5459e397c0965df69
Sam Russell <sam.russell@powershop.co.nz>
2015-09-23 15:50:29 +1200

    renamed again

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,4 @@
-require 'set'
+require "set"
 
 # This class holds the game state for a game of hangman
 # It can be initialised with a word in uppercase characters
@@ -9,19 +9,21 @@ require 'set'
 # - has a letter been guessed
 # - how many lives are left
 # The game state can be changed by guessing a letter
-class GameState
+class Game
   INITIAL_NUMBER_OF_LIVES = 9
   def initialize(initial_word_in_uppercase)
     @word = initial_word_in_uppercase
-    @masked_letters = (1..@word.size).map{nil}
     @guessed_letters = Set.new
   end
 
-  # will be def
-  attr_reader :masked_letters
-
   def lives_left
-    INITIAL_NUMBER_OF_LIVES - @guessed_letters.size + (@masked_letters.uniq - [nil]).size
+    INITIAL_NUMBER_OF_LIVES - @guessed_letters.size + (masked_letters.uniq - [nil]).size
+  end
+
+  def masked_letters
+    # this is how i would write this in python, doesn"t read nicely in english
+    # returns an array showing the word with un-guessed letters replaced with nil
+    @word.chars.map { |char| if @guessed_letters.include?(char) then char else nil end }
   end
 
   def has_letter_been_guessed?
@@ -29,18 +31,10 @@ class GameState
 
   def guess_letter(letter)
     @guessed_letters.add(letter)
-    #if @word.include?(letter)
-    #  .chars instead of .split('')
-    #  @word.split('').each_with_index do |val, index|
-    #    if val == letter
-    #      @masked_letters[index] = letter
-    #    end
-    #  end
-    #end
   end
 
   def won?
-    !@masked_letters.include?(nil)
+    !masked_letters.include?(nil)
   end
 
   def lost?
--- lib/random_word_generator.rb
+++ lib/random_word_generator.rb
@@ -1,17 +1,19 @@
 # this class returns random words
 class RandomWordGenerator
-  WORDLIST_FILENAME = './data/wordsEn.txt'
+  WORDLIST_FILENAME = "./data/wordsEn.txt"
   def initialize(seed = Random.new_seed)
     @seeded_random_number_generator = Random.new(seed)
   end
 
-  def get_random_word
-    #File.readlines('./data/wordsEn.txt')[@seeded_random_number_generator.rand(get_number_of_words)]
-    File.readlines('./data/wordsEn.txt')[rand(get_number_of_words)]
+  def random_word
+    #File.readlines("./data/wordsEn.txt")[@seeded_random_number_generator.rand(get_number_of_words)]
+    File.readlines("./data/wordsEn.txt")[rand(number_of_words)].chomp
   end
 
-  private def get_number_of_words
-    File.readlines('./data/wordsEn.txt').size
+  private
+  
+  def number_of_words
+    File.readlines("./data/wordsEn.txt").size
   end
 
 end
