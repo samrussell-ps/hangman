ab229f9efe83559afa7cc01197328adb66055c50
Sam Russell <sam.russell@powershop.co.nz>
2015-09-29 11:00:12 +1300

    more whitespace in run.rb

--- run.rb
+++ run.rb
@@ -5,5 +5,7 @@ require "./lib/user_talker"
 require "./lib/random_word_generator"
 
 game = Game.new(RandomWordGenerator.random_word.upcase)
+
 user_talker = UserTalker.new(game, ConsoleInterface.new)
+
 Controller.new(game, user_talker).run
